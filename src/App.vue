1bbd5517935239d08491b526ae4854c5db476148
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2021-06-01 14:19:27 +0530

    ➕ Redo Basics, add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoViewer msg="Test"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoViewer from './components/TodoViewer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoViewer
   }
 }
 </script>
---
+++ src/components/TodoViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todo-viewer">
+    <p class="font-sans">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoViewer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
